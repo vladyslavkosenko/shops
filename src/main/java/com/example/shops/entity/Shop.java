67df0e169cc23774828e70f2cb65e046085b2fac
vladyslavkosenko <vaduha91@gmail.com>
2022-09-07 22:52:12 +0300

    lombok

--- src/main/java/com/example/shops/entity/Shop.java
+++ src/main/java/com/example/shops/entity/Shop.java
@@ -1,5 +1,8 @@
 package com.example.shops.entity;
 
+import lombok.Data;
+
+@Data
 public class Shop {
     private String id;
     private String city;
@@ -7,62 +10,4 @@ public class Shop {
     private String name;
     private String employees;
     private Boolean website;
-
-    public Shop(String id, String city, String street, String name, String employees, Boolean website) {
-        this.id = id;
-        this.city = city;
-        this.street = street;
-        this.name = name;
-        this.employees = employees;
-        this.website = website;
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(String employees) {
-        this.employees = employees;
-    }
-
-    public Boolean getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(Boolean website) {
-        this.website = website;
-    }
 }
